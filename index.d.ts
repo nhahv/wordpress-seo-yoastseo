27fe561b52dd20b01ab5816ab420d36a5a7fa9fb
nhahv <nhahv.viss@gmail.com>
2022-08-06 11:46:12 +0700

    ok

--- index.d.ts
+++ index.d.ts
@@ -6,19 +6,20 @@ import TaxonomyAssessor from './types/scoring/taxonomyAssessor'
 import Pluggable from "./types/pluggable";
 import Researcher from "./types/languageProcessing/languages/_default/Researcher";
 import SnippetPreview from "./types/snippetPreview/snippetPreview"
+
 import Paper from './types/values/Paper';
 import AssessmentResult from "./types/values/AssessmentResult";
 import Assessment from "./types/scoring/assessments/assessment";
 
 import { AnalysisWorkerWrapper, createWorker, AnalysisWebWorker } from "./types/worker";
 
-
-import bundledPlugins 
-
-
-import helpers from './types/helpers';
-import AbstractResearcher from './types/languageProcessing/AbstractResearcher';
-
+import * as assessments from "./types/scoring/assessments";
+import * as bundledPlugins from "./types/bundledPlugins";
+import * as config from "./types/config";
+import * as helpers from "./types/helpers";
+import * as markers from "./types/markers";
+import * as string from "./types/stringProcessing";
+import * as interpreters from "./types/scoring/interpreters";
 
 export {
     App,
